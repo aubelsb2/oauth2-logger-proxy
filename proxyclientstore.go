457c30224b93ea1b1a352e1913775459b0a7f50f
arran ubels <arran4@gmail.com>
2020-07-07 09:58:16 +1000

    Constructor objects to hidden objects.

--- proxyclientstore.go
+++ proxyclientstore.go
@@ -6,11 +6,15 @@ import (
 	"log"
 )
 
-type ProxyClientStore struct {
+type proxyClientStore struct {
 	clientStore oauth2.ClientStore
 }
 
-func (pcs ProxyClientStore) GetByID(ctx context.Context, id string) (ci oauth2.ClientInfo, err error) {
+func ProxyClientStore(cs oauth2.ClientStore) oauth2.ClientStore {
+	return &proxyClientStore{clientStore: cs}
+}
+
+func (pcs proxyClientStore) GetByID(ctx context.Context, id string) (ci oauth2.ClientInfo, err error) {
 	log.Printf("GetByID(%#v)", id)
 	ci, err = pcs.clientStore.GetByID(ctx, id)
 	log.Printf("GetByID() = %v, %v", ci, err)
